886d6d93f33f491d3a477b0a33b3deb1f964b640
groesie-2 <bolatusd@yandex.ru>
2023-12-09 19:09:40 +0300

    minor fixes

--- proccess_chat.py
+++ proccess_chat.py
@@ -124,7 +124,7 @@ async def audio_answer(answer: UserAnswer, request: Request):
 
 
         ts = int(time.time())
-        tts_response.stream_to_file(f'./{auth_token}_{ts}.mp3')
+        # tts_response.stream_to_file(f'./{auth_token}_{ts}.mp3')
         # response = requests.get(audio_url, stream=True)
         return StreamingResponse(tts_response.iter_bytes(chunk_size=1024), media_type="audio/mpeg")
         return {'ts': 'ts3'}
